0f4c81faf3bdaee441d017bee840913e0cea29df
raynardj <b2ray2c@gmail.com>
2024-12-03 10:55:04 -0800

    simple popup manual

---
+++ readly-chrome/js/popup.js
@@ -0,0 +1,7 @@
+document.querySelector('#readButton').addEventListener('click', () => {
+    window.open('read.html', '_blank');
+});
+
+document.querySelector('#settingsButton').addEventListener('click', () => {
+    window.open('settings.html', '_blank');
+});
